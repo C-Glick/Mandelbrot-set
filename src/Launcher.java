2d24998e4e0252891181eb7b6004d78d687f78ec
TheChosenHero <thechosenherotch@gmail.com>
2018-12-27 14:23:01 -0600

    added basic display window

--- src/Launcher.java
+++ src/Launcher.java
@@ -4,6 +4,8 @@ public class Launcher {
 	public static void main(String[] args) {
 		Complex a = new Complex(0,0.27);
 		Tester tester = new Tester();
+		Display display = new Display();
+		display.init();
 		System.out.println(a.abs());
 		
 		System.out.println(tester.test1(a));
