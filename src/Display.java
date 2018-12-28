0b217e402792112d3b65c26fc2274a975b433491
colton_glick <colton_glick@Colton-XPS>
2018-12-28 11:05:18 -0600

    display update

--- src/Display.java
+++ src/Display.java
@@ -1,22 +1,60 @@
 import java.awt.Canvas;
+import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
+
 import javax.swing.JFrame;
+//this class handles the display window that the game runs it
+public class Display {
 
-public class Display extends Canvas{
-	Graphics g;
+	private String title;
+	private int width, height;
 	
+	private JFrame frame;
+	private Canvas canvas;
+	private Graphics g;
+	private BufferStrategy bs;
 	
-	public void init() {									//draws graphics to the screen for user
-		JFrame frame = new JFrame("My Drawing");
-        Canvas canvas = new Display();
-        canvas.setSize(400, 400);
-        frame.add(canvas);
-        frame.pack();
-        frame.setVisible(true);
-    }
-
-    public void paint(Graphics g) {
-        g.fillOval(100, 100, 200, 200);		
+	public Display(String title, int width, int height) {
+		this.title = title;
+		this.width = width;
+		this.height =height;
+		
+		createDisplay();
 	}
-
+	
+	//Initialize the display and all its parameters
+	private void createDisplay() {
+		frame = new JFrame(title);
+		frame.setSize(width, height);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setResizable(false);
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
+		
+		//create a new canvas for the graphics to be drawn on
+		canvas = new Canvas();
+		canvas.setPreferredSize(new Dimension(width,height));
+		canvas.setMaximumSize(new Dimension(width,height));
+		canvas.setMinimumSize(new Dimension(width,height));
+		canvas.setFocusable(false);
+		
+		frame.add(canvas);
+		frame.pack();				//packs the frame so the canvas fits appropriately
+	}
+	
+	public Graphics initDisplay() {
+		bs = this.getCanvas().getBufferStrategy();		//gets buffer strat from canvas, if non exists set it
+		if(bs==null) {
+			this.getCanvas().createBufferStrategy(3);
+		}
+		g = bs.getDrawGraphics();							//get the graphics drawer from the buffer strat
+		return g;
+	}
+	public Canvas getCanvas() {
+		return canvas;
+	}
+	public JFrame getFrame() {
+		return frame;
+	}
 }
--- src/Launcher.java
+++ src/Launcher.java
@@ -1,14 +1,18 @@
+import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
 
 public class Launcher {
 
 	public static void main(String[] args) {
-		Complex a = new Complex(0,0.27);
-		Tester tester = new Tester();
-		Display display = new Display();
-		display.init();
-		System.out.println(a.abs());
+		Display display = new Display("title",500,500);
+		Graphics g = display.initDisplay();
 		
-		System.out.println(tester.test1(a));
+		
+		//Complex a = new Complex(0,0.27);
+		//Tester tester = new Tester();
+		//System.out.println(a.abs());
+		
+		//System.out.println(tester.test1(a));
 	}
 
 }
